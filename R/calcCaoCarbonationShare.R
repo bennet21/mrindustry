cc956ae6af221c2e46572cd351f4eff644b31970
Bennet Weiss <bennet.weiss@pik-potsdam.de>
2025-08-15 18:52:43 +0200

    rename CaO function

--- R/calcCaoCarbonationShare.R
+++ R/calcCaoCarbonationShare.R
@@ -1,13 +1,13 @@
 #' Calculate share of cao available for carbonation.
 #'
 #' @author Bennet Weiss
-calcCaoCarbonationShare <- function(){
+calcCaOCarbonationShare <- function(){
   x <- readSource("Cao2024", subtype = "cao_carbonation_share")
 
   # create new magpie object and fill with ones
   weight <- new.magpie(cells_and_regions = NULL)
   weight <- toolCountryFill(weight, fill = 1, verbosity = 2)
-
+  
   unit <- "ratio"
   description <- paste(
     "Share of CaO in End-Use Product available for carbonation.",
